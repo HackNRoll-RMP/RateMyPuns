d4224ca167db401564e92c270a333b4b817560a7
LawMan <lawrenciok@gmail.com>
2018-01-27 14:54:03 +0800

    add schema to Puns collection

--- imports/api/puns.js
+++ imports/api/puns.js
@@ -1,3 +1,20 @@
 import { Mongo } from 'meteor/mongo';
  
 export const Puns = new Mongo.Collection('puns');
+
+let PunsSchema = new SimpleSchema({
+	'owner': {
+		type: String,
+		label: 'The ID of the user that created the pun.'
+	},
+	'pun': {
+		type: String,
+		label: 'The content of the pun.'
+	},
+	'timestamp': {
+		type: Date,
+		label: 'The date and time the pun was created.'
+	}
+});
+
+Puns.attachSchema(PunsSchema);
