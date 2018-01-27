46d2adb96bf97b52125b9699fff5f273681ca001
Jaivignesh Venugopal <smugs1995@gmail.com>
2018-01-27 21:05:16 +0800

    Add functionality to logout

--- client/dashboard/dashboard.js
+++ client/dashboard/dashboard.js
@@ -4,13 +4,20 @@ import { Puns } from '../../imports/api/puns.js';
 import './dashboard.html';
 
 Template.dashboard.events({
+
+  'click .logout' :function(event) {
+    event.preventDefault();
+    Meteor.logout();
+    Router.go('/');
+  },
+
   'submit .new-pun'(event, template) {
     // Prevent default browser form submit
     event.preventDefault();
- 
+
     // Get value from form element
     var text = template.find('#pun').value;
- 
+
     // Insert a task into the collection
     Puns.insert({
       owner: Meteor.userId(),
